3ee35bdfde4076f1db79382bb9e29713921f42db
Joey Alan <joeyalan50@gmail.com>
2024-03-01 21:59:28 -0300

    ### changed - Changed class name of Main to Controller

--- src/main/java/mainControllerFront/Controller.java
+++ src/main/java/mainControllerFront/Controller.java
@@ -7,7 +7,7 @@ import javafx.scene.control.*;
 import java.util.List;
 import java.util.Map;
 
-public class Controlle{
+public class Controller{
 
     @FXML
     private TitledPane titledPaneModels;
