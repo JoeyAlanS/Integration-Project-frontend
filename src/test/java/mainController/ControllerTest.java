1a13b1347288803333569520b6636fc92524d412
Joey alan <joeyalan50@gmail.com>
2024-07-24 18:22:55 -0300

    ###Added - Added new tests

--- src/test/java/mainController/ControllerTest.java
+++ src/test/java/mainController/ControllerTest.java
@@ -72,7 +72,6 @@ public class ControllerTest extends ApplicationTest {
         Mockito.when(mainController.lineupService.getAllLineup()).thenReturn(mockList);
 
         // When
-        doNothing().when(mainController).comboBoxSelect();
         mainController.comboBoxSelect();
         LineupDTO selectedItem = mockList.get(0);
         mainController.comboBox.getSelectionModel().select(selectedItem);
@@ -86,6 +85,40 @@ public class ControllerTest extends ApplicationTest {
         mainController.comboBox.valueProperty().set(null);
         verify(mainController, times(1)).openTreeView(null);
 
+        mainController.comboBox.valueProperty().set(selectedItem);
+        verify(mainController, times(2)).openTreeView(selectedItem);
+    }
+
+    @Test
+    public void comboBoxSelectEmptyTest() {
+        // Given
+        List<LineupDTO> emptyList = new ArrayList<>();
+        Mockito.when(mainController.lineupService.getAllLineup()).thenReturn(emptyList);
+
+        // When
+        mainController.comboBoxSelect();
+
+        // Then
+        assertEquals(FXCollections.observableArrayList(emptyList), mainController.comboBox.getItems());
+        verify(mainController.lineupService).getAllLineup();
+    }
+
+    @Test
+    public void comboBoxSelectListenerTest() {
+        // Given
+        List<LineupDTO> mockList = new ArrayList<>();
+        mockList.add(new LineupDTO("Ares", (short) 1));
+        mockList.add(new LineupDTO("Cronos", (short) 2));
+        Mockito.when(mainController.lineupService.getAllLineup()).thenReturn(mockList);
+
+        // When
+        mainController.comboBoxSelect();
+        LineupDTO selectedItem = mockList.get(0);
+        mainController.comboBox.getSelectionModel().select(selectedItem);
+        mainController.comboBox.valueProperty().set(null);
+
+        // Then
+        verify(mainController, times(1)).openTreeView(null);
         mainController.comboBox.valueProperty().set(selectedItem);
         verify(mainController, times(2)).openTreeView(selectedItem);
     }
